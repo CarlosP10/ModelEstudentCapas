31b93f8e6308ae131d4208a1bfbb4bd947c77358
estherlaraaaa <37164243+estherlaraaaa@users.noreply.github.com>
2020-06-27 18:06:22 -0600

    Agregando vistas

--- src/main/java/com/uca/mestudiantes/configuration/JPAConfiguration.java
+++ src/main/java/com/uca/mestudiantes/configuration/JPAConfiguration.java
@@ -35,7 +35,7 @@ public class JPAConfiguration {
 		dataSource.setDriverClassName("org.postgresql.Driver");
 		dataSource.setUrl("jdbc:postgresql://localhost:5432/MEstudiantes");
 		dataSource.setUsername("postgres");
-		dataSource.setPassword("123456");
+		dataSource.setPassword("Adiosjeje1");
 		return dataSource;
 	}
 	
