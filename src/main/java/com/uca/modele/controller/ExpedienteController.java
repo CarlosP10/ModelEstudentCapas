b7a85f87f023ca84b846bbb47ea3a169f614e068
estherlaraaaa <37164243+estherlaraaaa@users.noreply.github.com>
2020-07-02 00:39:22 -0600

    materiaController y expedienteController, falta probarlos!

---
+++ src/main/java/com/uca/modele/controller/ExpedienteController.java
@@ -0,0 +1,53 @@
+package com.uca.modele.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.uca.modele.domain.Expediente;
+import com.uca.modele.domain.Materias;
+import com.uca.modele.service.ExpedienteService;
+
+
+public class ExpedienteController {
+	
+	@Autowired
+	private ExpedienteService expedienteService;
+	
+	@RequestMapping("/pantallaInicialExpediente")
+	public String pantallaExp() {
+		return "expedienteA";
+	}
+	
+	@RequestMapping("/verExpediente")
+	public @ResponseBody List<Expediente> verExpediente() {
+		Sort sort = Sort.by(Direction.ASC, "ccliente");
+		return expedienteService.findAll(sort);
+	}
+
+	@RequestMapping("/editarExpediente")
+	public ModelAndView buscar(@RequestParam Integer id) {
+		ModelAndView mav = new ModelAndView();
+		Expediente c = expedienteService.findOne(id);
+		mav.addObject("expediente", c);
+		mav.setViewName("modificarExp");
+		return mav;
+	}
+
+	@RequestMapping("/guardarExpediente")
+	public ModelAndView guardarExpediente(@ModelAttribute Expediente expedientes) {
+		ModelAndView mav = new ModelAndView();
+		expedienteService.save(expedientes);
+		mav.setViewName("busquedaA");
+		mav.addObject("resultado", 1);
+		return mav;
+	}
+
+}
--- src/main/java/com/uca/modele/controller/MateriaController.java
+++ src/main/java/com/uca/modele/controller/MateriaController.java
@@ -3,8 +3,13 @@ package com.uca.modele.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 
 import com.uca.modele.dao.MateriasDAO;
 import com.uca.modele.domain.Materias;
@@ -18,25 +23,33 @@ public class MateriaController {
 	@Autowired
 	MateriasDAO materiasDAO;
 
-	// VISTA PARA MATERIA
-	@RequestMapping("/materia")
-	public ModelAndView verMateria() {
+	@RequestMapping("/materiasTable")
+	public String materiasTable() {
+		return "catalogoM";
+	}
+
+	@RequestMapping("/verMaterias")
+	public @ResponseBody List<Materias> verMaterias() {
+		Sort sort = Sort.by(Direction.ASC, "ccliente");
+		return materiasService.findAll(sort);
+	}
+
+	@RequestMapping("/editarMateria")
+	public ModelAndView buscar(@RequestParam Integer id) {
 		ModelAndView mav = new ModelAndView();
-		Materias materias = new Materias();
-		List<Materias> materiass = null;
-		try {
-			materiass = materiasService.findAll();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		mav.addObject("error", "");
-
-		mav.addObject("materiass", materiass);
-		mav.addObject("materias", materias);
-		mav.setViewName("catalogoM");
+		Materias c = materiasService.findOne(id);
+		mav.addObject("materias", c);
+		mav.setViewName("FALTA HACER EL HTML DE EDITAR MATERIAS");
 		return mav;
 	}
-	
-	
+
+	@RequestMapping("/guardarMaterias")
+	public ModelAndView guardarMaterias(@ModelAttribute Materias materias) {
+		ModelAndView mav = new ModelAndView();
+		materiasService.save(materias);
+		mav.setViewName("catalogoM");
+		mav.addObject("resultado", 1);
+		return mav;
+	}
+
 }
