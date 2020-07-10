10279855d70e261ca8867166c6746eed4e9a860b
estherlaraaaa <37164243+estherlaraaaa@users.noreply.github.com>
2020-07-09 20:55:57 -0600

    proyecto terminado

--- src/main/java/com/uca/modele/controller/MainController.java
+++ src/main/java/com/uca/modele/controller/MainController.java
@@ -21,13 +21,6 @@ import com.uca.modele.service.CuentaService;
 @Controller
 public class MainController {
 
-//	@RequestMapping("/login")
-	//public ModelAndView indexLaboratorio8() {
-		//ModelAndView mav = new ModelAndView();
-		//mav.setViewName("login");
-	//	return mav;
-	//}
-
 	@Autowired
 	CuentaService cuentaService; 
 	
@@ -36,24 +29,8 @@ public class MainController {
 		return "login2";
 	}
 	
-//	//mostrar pantalla inicial de login
-//	@RequestMapping("/iniciarSesion") //estaba en index ojo
-//	public ModelAndView index(HttpSession request) {
-//		ModelAndView mav = new ModelAndView();
-//		Cuenta cuentas = null;
-//		if (request.getAttribute("nombre_usuario") != null) {
-//			cuentas = (Cuenta) request.getAttribute("nombre_usuario");
-//			if (cuentas.getIdTipo().getIdTipo() == 1) {
-//				mav.setViewName("redirect:/indexAdmin");
-//			} else {
-//				mav.setViewName("redirect:/indexCoordi");
-//			}
-//		} else {
-//			mav.setViewName("/login");
-//		}
-//		return mav;
-//	}
 	
+	//iniciar sesion
 	@RequestMapping("/index2")
 	public ModelAndView index2(@RequestParam String nombreusuario,@RequestParam String contrasenia)throws Exception {
 		ModelAndView mav = new ModelAndView();
@@ -67,91 +44,54 @@ public class MainController {
 		return mav;
 	};
 	
-	//serrar sesion de usuario
-//	@RequestMapping("/logout")
-//	public ModelAndView logout(HttpSession request) {
-//		ModelAndView mav = new ModelAndView();
-//		Cuenta cuentas = null;
-//		try {
-//			cuentas = (Cuenta) request.getAttribute("nombre_usuario");
-//			cuentas.setSesion(false);
-//			cuentaService.save(cuentas);
-//			request.removeAttribute("nombre_usuario");
-//			mav.setViewName("redirect:/index");
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-//		return mav;
-//	}
-
-	
-//	//iniciar sesion 
-//	@RequestMapping("/iniciarSesion")
-//	public ModelAndView iniciarSesion(@Valid @ModelAttribute Cuenta cuenta, BindingResult result, HttpSession request) {
-//		ModelAndView mav = new ModelAndView();
-//		Cuenta cuentas = null;
-//		try {
-//			cuentas = cuentaService.login(cuenta.getNombreUsuario(), cuenta.getContrasenia());
-//			if (cuentas == null) {
-//				mav.setViewName("index");
-//				mav.addObject("mensaje", "Su usuario y/o contraseña son incorrectos!");
-//			} else {
-//				if (cuentas.getEstado() == true) {
-//					if (cuentas.getSesion() == false) {
-//						if (cuentas.getIdTipo().getIdTipo() == 1) {
-//							mav.setViewName("redirect:/indexAdmin");
-//						} else {
-//							mav.setViewName("redirect:/indexCoordi");
-//						}
-//						cuentas.setSesion(true);
-//						cuentaService.save(cuentas);
-//						request.setAttribute("nombre_usuario", cuentas);
-//					} else {
-//						mav.setViewName("index");
-//						mav.addObject("mensaje", "Su sesión ya esta activa!");
-//					}
-//				} else {
-//					mav.setViewName("index");
-//					mav.addObject("mensaje", "Su cuenta no ha sido activada!");
-//				}
-//			}
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-//		return mav;
-//	}
+	//cerrar sesion de usuario
+	@RequestMapping("/logout")
+	public ModelAndView logout(HttpSession request) {
+		ModelAndView mav = new ModelAndView();
+		Cuenta cuentas = null;
+		try {
+			cuentas = (Cuenta) request.getAttribute("nombre_usuario");
+			cuentas.setSesion(false);
+			cuentaService.save(cuentas);
+			request.removeAttribute("nombre_usuario");
+			mav.setViewName("redirect:/index");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return mav;
+	}
 	
 	
-//	//para validar la sesion del admin (aun no se implemento)
-//	public ModelAndView sesionAdmin(HttpSession request, ModelAndView mav) {
-//		Cuenta user = null;
-//		user = (Cuenta) request.getAttribute("user");
-//		if (user != null) {
-//			if (user.getIdTipo().getIdTipo() == 1) {
-//				mav.clear();
-//				mav.setViewName("redirect:/indexAdmin");
-//			}
-//		} else {
-//			mav.clear();
-//			mav.setViewName("redirect:/index");
-//		}
-//		return mav;
-//	}
-//	
-//	//validar sesion del coordi (aun no se implemento)
-//	public ModelAndView sesionCoordinador(HttpSession request, ModelAndView mav) {
-//		Cuenta user = null;
-//		user = (Cuenta) request.getAttribute("user");
-//		if (user != null) {
-//			if (user.getIdTipo().getIdTipo() == 2) {
-//				mav.clear();
-//				mav.setViewName("redirect:/indexCoordi");
-//			}
-//		} else {
-//			mav.clear();
-//			mav.setViewName("redirect:/index");
-//		}
-//		return mav;
-//	}
+	//para validar la sesion del admin 
+	public ModelAndView sesionAdmin(HttpSession request, ModelAndView mav) {
+		Cuenta user = null;
+		user = (Cuenta) request.getAttribute("user");
+		if (user != null) {
+			if (user.getIdTipo().getIdTipo() == 1) {
+				mav.clear();
+				mav.setViewName("redirect:/indexAdmin");
+			}
+		} else {
+			mav.clear();
+			mav.setViewName("redirect:/index");
+		}
+		return mav;
+	}
+	
+	//validar sesion del coordi 
+	public ModelAndView sesionCoordinador(HttpSession request, ModelAndView mav) {
+		Cuenta user = null;
+		user = (Cuenta) request.getAttribute("user");
+		if (user != null) {
+			if (user.getIdTipo().getIdTipo() == 2) {
+				mav.clear();
+				mav.setViewName("redirect:/indexCoordi");
+			}
+		} else {
+			mav.clear();
+			mav.setViewName("redirect:/index");
+		}
+		return mav;
+	}
 
 }
